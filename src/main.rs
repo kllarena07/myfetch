6836a5375a94340ff6b1049eb783404f8dadfc14
Kieran Llarena <baymax709@gmail.com>
2024-11-29 11:43:18 -0500

    feat: print ascii block img to terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use rascii_art::{
+    render_to,
+    RenderOptions,
+};
+                                                            
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = String::new();
+                                                            
+    render_to(
+        r"/Users/kllarena/documents/github/myfetch/mental_illness.png",
+        &mut buffer,
+        &RenderOptions::new()
+            .width(100)
+            .colored(true)
+            .charset(rascii_art::charsets::BLOCK)
+    )
+    .unwrap();
+
+    println!("{}", buffer);
 }
